9a0523a82eb98600b20dfd5ea7382b88f8398734
PaooRojas <valverde.rv.15@gmail.com>
2021-05-24 20:08:49 -0600

    Clase Yorleny

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,11 +1,13 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Paola;
+import com.ucreativa.familia.Yorleny;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Paola yo = new Paola();
+        Yorleny mama = new Yorleny("Hola");
     }
 }
 
---
+++ src/com/ucreativa/familia/Yorleny.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class Yorleny {
+    public Yorleny(String saludo) {
+        System.out.println(saludo);
+
+    }
+}
