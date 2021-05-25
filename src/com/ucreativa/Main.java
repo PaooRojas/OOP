3f2b4648c4902646590468455941273c56223a8d
PaooRojas <valverde.rv.15@gmail.com>
2021-05-24 21:55:28 -0600

    Clase Yorleny

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -6,8 +6,19 @@ import com.ucreativa.familia.Yorleny;
 public class Main {
 
     public static void main(String[] args) {
-	    Paola yo = new Paola();
+
+        Paola yo = new Paola();
+
         Yorleny mama = new Yorleny("Hola");
+
+        mama.setAge();
+
+        int age = mama.getAge();
+        System.out.println("My age is" + age);
+        System.out.println("Amo" + mama.getHobby());
+
+
+        System.out.println("Chao" + age);
     }
 }
 
--- src/com/ucreativa/familia/Yorleny.java
+++ src/com/ucreativa/familia/Yorleny.java
@@ -1,8 +1,30 @@
 package com.ucreativa.familia;
 
 public class Yorleny {
+
+    private int age;
+
+    private String hobby;
+
+
     public Yorleny(String saludo) {
+        this.age = 47;
+        this.hobby = "Gardening";
         System.out.println(saludo);
+    }
+
+    public void setAge(){
+        this.age = this.age + 1;
 
     }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+
 }
