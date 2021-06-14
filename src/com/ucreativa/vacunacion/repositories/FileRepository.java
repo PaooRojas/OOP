ba551bfff679ca004da7d11f9b0f3a0810053487
PaooRojas <valverde.rv.15@gmail.com>
2021-06-13 20:40:33 -0600

    File Repository

--- src/com/ucreativa/vacunacion/repositories/FileRepository.java
+++ src/com/ucreativa/vacunacion/repositories/FileRepository.java
@@ -2,21 +2,23 @@ package com.ucreativa.vacunacion.repositories;
 
 import com.ucreativa.vacunacion.entities.Persona;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileRepository implements Repository {
+
+    private final String FILE_PATH = "db.txt";
+
     @Override
     public void save(Persona persona, String sintomas, String marca, int dosis, Date fecha) {
 
         SimpleDateFormat format = new SimpleDateFormat("dd,MM,yyyy hh:mm:ss");
         String text = persona.getNombre() + " " + marca + " " + format.format(fecha) + "\n";
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("db.txt", true));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true));
             writer.append(text);
             writer.close();
         } catch (IOException e) {
@@ -27,7 +29,12 @@ public class FileRepository implements Repository {
 
     @Override
     public List<String> get() {
-
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH));
+            return reader.lines().collect(Collectors.toList());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
